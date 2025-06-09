9013110aa1389dc2a4d657c981fafd4c1c5452e4
bnmjol <bnm20x24@gmail.com>
2025-06-09 09:41:09 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center p-6 bg-gradient-to-br from-white to-cyan-100">
-      <h1 className="text-2xl font-bold">مرحبًا بك في Chalengeaz 👋</h1>
-    </main>
-  );
-}
