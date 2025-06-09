bb51cde3418276d2c2fd171980c05b6168c934ee
bnmjol <bnm20x24@gmail.com>
2025-06-09 09:49:38 +0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40, textAlign: 'center' }}>
-      <h1>🎮 أهلاً بك في Chalengeaz</h1>
-      <p>المنصة قيد التطوير...</p>
-    </main>
-  );
-}
